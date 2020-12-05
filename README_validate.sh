a65f8165e0f3eb8735abbf9143f3d5bde3e1bd03
harunsvnc <75423540+harunsvnc@users.noreply.github.com>
2020-12-06 00:22:08 +0300

    Update README_validate.sh

--- README_validate.sh
+++ README_validate.sh
@@ -1,2 +1,2 @@
 #validate.sh
-reads mails from a file and sends results to screen about mail format.
+reads mails from a file and sends results to screen about mail format. A file is needed that contains lines of mail address.(wordlist.txt)
